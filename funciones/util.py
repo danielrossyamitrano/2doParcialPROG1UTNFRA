9c49c87549ece589cc78b860e31179a057434303
danielrossyamitrano <danielrossyamitrano@gmail.com>
2025-06-30 19:25:58 -0300

    Subida la imagen de mafalda y la función cargar imagen.

--- funciones/util.py
+++ funciones/util.py
@@ -1,4 +1,12 @@
+from pygame import image
+from os import path
+
 def abrir_txt(filepath):
     with open(filepath, 'rt', encoding='utf-8') as file:
         lineas = [line.rstrip('\n') for line in file.readlines()]
     return lineas
+
+def cargar_imagen(ruta):
+    if path.exists(ruta):
+        imagen = image.load(ruta).convert_alpha()
+        return imagen
